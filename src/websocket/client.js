99ba303352ceced0dfb0bf6a156e41621d904f13
潘前进 <panqianjin@panqianjindeMacBook-Pro.local>
2017-10-23 09:48:40 +0800

    update

--- src/websocket/client.js
+++ src/websocket/client.js
@@ -1,18 +1,42 @@
 // Create WebSocket connection.
 var socket = new WebSocket('ws://localhost:8080');
-
+var id = Math.floor(Math.random () * 1000)
+function query(selecter){
+    return document.querySelector(selecter)
+}
+function queryAll(selecter){
+    return document.querySelectorAll(selecter)
+}
 // Connection opened
 socket.addEventListener('open', function (event) {
-    socket.send(JSON.stringify('Hello Server!'));
-    // socket.send(JSON.stringify({
-    //     event: 'join',
-    //     data: {
-    //       room: 1
-    //     }
-    //   }))
+    socket.send(JSON.stringify({
+        event:'join',
+        data:{
+            id:id,
+            name:'user'+id
+        }
+    }));
 });
 
 // Listen for messages
 socket.addEventListener('message', function (event) {
     console.log('Message from server', event.data);
+    var data = JSON.parse(event.data)
+    if(data.id && data.id == id){
+        render(data)
+    }else{
+        query('.show').innerHTML = data
+    }
 });
+function render(data){
+    this.num.innerHTML = data.nums
+    this.content.innerHTML = data.show 
+}
+function init(){
+    this.num = query('#num');
+    this.content = query('.show');
+    this.nums = 0;
+    this.show = '';
+    render(this)
+}
+init()
--- src/websocket/server.js
+++ src/websocket/server.js
@@ -1,12 +1,27 @@
+// in your nodeserver file
 var ws = require('node-websocket').init;
-var sockets = []
+var sockets = [],
+users = {}
 // bind events
 ws.on('connection', function(socket) {
   console.log(socket.id); // ID in cookie,key: socketId
-  sockets.push(socket.id)
   ws.on('join', function(data) {
-    //send message
-    socket.send('yes! it will send to frontend');
+    console.log(data)
+    var obj = data
+    console.log(obj)
+    if(obj.id && obj.name){
+      var id = obj.id,
+          name = obj.name
+      socket.send(JSON.stringify('欢迎'+obj.name+'登录'));
+      users[obj.id] = obj.name
+    setTimeout(function(){
+      socket.send(JSON.stringify({
+        nums:1,
+        show:'尊敬的用户'+name+'请注意，现在要吃饭了',
+        id:id
+      }));
+    },3000)
+  }
   });
 });
 
